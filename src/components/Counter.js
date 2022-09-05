20517c32ec8034fe93e7918c44e2ee2a83d69eeb
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-09-05 03:13:54 +0100

    Added basic counter logic

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Counter() {
-  return (
-    <div>Counter</div>
-  )
+    const [count, setCount] = useState(0);
+    const incrementAmount = 1;
+    const decrementAmount = 1;
+
+    const incrementCount = () => {
+        setCount(count + incrementAmount);
+    }
+
+    const decrementCount = () => {
+        setCount(count - decrementAmount);
+    }
+
+    const resetCount = () => {
+        setCount(0);
+    }
+
+    return (
+        <div>
+            <h1>Counter</h1>
+            <button onClick={decrementCount}>-</button>
+            <span>Count value: { count }</span>
+            <button onClick={incrementCount}>+</button>
+            <br />
+            <button onClick={resetCount}>Reset</button>
+        </div>
+    )
 }
 
 export default Counter;
